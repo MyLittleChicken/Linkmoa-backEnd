7a734431555ad6c0d31df6802adfa214491cf4ba
parkjihyeon124594 <91263647+parkjihyeon124594@users.noreply.github.com>
2025-04-21 18:45:22 +0900

    LP-175 refactor : SiteDetailResponse와 DirectoryDetailResponse에 @Getter 추가

--- src/main/java/com/linkmoa/source/domain/site/dto/response/SiteDetailResponse.java
+++ src/main/java/com/linkmoa/source/domain/site/dto/response/SiteDetailResponse.java
@@ -1,8 +1,10 @@
 package com.linkmoa.source.domain.site.dto.response;
 
+import lombok.Getter;
 import lombok.experimental.SuperBuilder;
 
 @SuperBuilder
+@Getter
 public class SiteDetailResponse extends SiteSimpleResponse {
 	private Integer orderIndex;
 }
