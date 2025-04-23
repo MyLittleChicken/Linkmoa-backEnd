90c3fad56a9b2be53bf6f3e4c0c0505b61d57fb5
parkjihyeon124594 <91263647+parkjihyeon124594@users.noreply.github.com>
2025-04-23 16:21:57 +0900

    LP-180 feat : 공유 페이지 대시보드 멤버 조회 QueryDSL 추가

--- src/main/java/com/linkmoa/source/domain/page/repository/PageDataAccess.java
+++ src/main/java/com/linkmoa/source/domain/page/repository/PageDataAccess.java
@@ -3,6 +3,7 @@ package com.linkmoa.source.domain.page.repository;
 import java.util.List;
 import java.util.Optional;
 
+import com.linkmoa.source.domain.page.dto.response.PageDashboardMemberDto;
 import com.linkmoa.source.domain.page.dto.response.PageResponse;
 import com.linkmoa.source.domain.page.entity.Page;
 
@@ -23,5 +24,9 @@ public interface PageDataAccess {
 	Long findRootDirectoryIdByPageId(Long pageId);
 
 	String findPageTitleById(Long pageId);
+
+	List<PageDashboardMemberDto> findDashboardMembersByPageId(Long pageId);
+
+	List<PageDashboardMemberDto> findWaitingInvitedMembersByPageId(Long pageId);
 
 }
--- src/main/java/com/linkmoa/source/domain/page/repository/rdb/PageQueryDslRepositoryImpl.java
+++ src/main/java/com/linkmoa/source/domain/page/repository/rdb/PageQueryDslRepositoryImpl.java
@@ -1,5 +1,7 @@
 package com.linkmoa.source.domain.page.repository.rdb;
 
+import static com.linkmoa.source.domain.dispatch.entity.QSharePageInvitationRequest.*;
+import static com.linkmoa.source.domain.member.entity.QMember.*;
 import static com.linkmoa.source.domain.memberPageLink.entity.QMemberPageLink.*;
 import static com.linkmoa.source.domain.page.entity.QPage.*;
 
@@ -7,9 +9,12 @@ import java.util.List;
 
 import org.springframework.stereotype.Repository;
 
+import com.linkmoa.source.domain.dispatch.constant.RequestStatus;
+import com.linkmoa.source.domain.page.dto.response.PageDashboardMemberDto;
 import com.linkmoa.source.domain.page.dto.response.PageResponse;
 import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
 import lombok.RequiredArgsConstructor;
@@ -46,5 +51,39 @@ public class PageQueryDslRepositoryImpl {
 			.from(page)
 			.where(page.id.eq(pageId))
 			.fetchOne();
+	}
+
+	public List<PageDashboardMemberDto> findDashboardMembersByPageId(Long pageId) {
+		return jpaQueryFactory
+			.select(Projections.constructor(
+				PageDashboardMemberDto.class,
+				member.id,
+				member.email,
+				member.nickname,
+				memberPageLink.permissionType.stringValue(),
+				Expressions.constant(false)
+			))
+			.from(memberPageLink)
+			.join(memberPageLink.member, member)
+			.where(memberPageLink.page.id.eq(pageId))
+			.fetch();
+	}
+
+	public List<PageDashboardMemberDto> findWaitingInvitedMembersByPageId(Long pageId) {
+		return jpaQueryFactory
+			.select(Projections.constructor(
+				PageDashboardMemberDto.class,
+				sharePageInvitationRequest.receiver.id,
+				sharePageInvitationRequest.receiver.email,
+				sharePageInvitationRequest.receiver.nickname,
+				Expressions.constant(null),
+				Expressions.constant(true)
+			))
+			.from(sharePageInvitationRequest)
+			.where(
+				sharePageInvitationRequest.page.id.eq(pageId),
+				sharePageInvitationRequest.requestStatus.eq(RequestStatus.WAITING)
+			)
+			.fetch();
 	}
 }
