e29d34488cb8497f83bafd1eec3517a3da98647b
parkjihyeon124594 <91263647+parkjihyeon124594@users.noreply.github.com>
2025-04-26 11:49:46 +0900

    LP-184 feat : SharePageDashboardPermissionUpdateDto에 validation 어노테이션 추가

--- src/main/java/com/linkmoa/source/domain/page/dto/request/SharePageDashboardPermissionUpdateDto.java
+++ src/main/java/com/linkmoa/source/domain/page/dto/request/SharePageDashboardPermissionUpdateDto.java
@@ -3,14 +3,16 @@ package com.linkmoa.source.domain.page.dto.request;
 import com.linkmoa.source.domain.memberPageLink.constant.PermissionType;
 import com.linkmoa.source.global.dto.request.BaseRequest;
 
+import jakarta.annotation.Nullable;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 
 public class SharePageDashboardPermissionUpdateDto {
 
 	public record Request(
 		BaseRequest baseRequest,
-		Long targetMemberId,             // 권한 변경 또는 내보내기 대상 사용자의 ID
-		PermissionType permissionType    // 설정할 권한 (null일 경우 내보내기로 간주)
+		@NotNull Long targetMemberId,             // 권한 변경 또는 내보내기 대상 사용자의 ID
+		@Nullable PermissionType permissionType    // 설정할 권한 (null일 경우 내보내기로 간주)
 	) {
 
 	}
