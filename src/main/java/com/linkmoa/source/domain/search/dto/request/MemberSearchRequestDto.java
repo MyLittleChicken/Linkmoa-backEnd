d091db11b95d96b09d0131f03c08e33aa1431588
parkjihyeon124594 <91263647+parkjihyeon124594@users.noreply.github.com>
2025-04-29 16:12:38 +0900

    LP-186 fix: MemberSearchRequestDto Request 필드명 keyword로 수정

--- src/main/java/com/linkmoa/source/domain/search/dto/request/MemberSearchRequestDto.java
+++ src/main/java/com/linkmoa/source/domain/search/dto/request/MemberSearchRequestDto.java
@@ -7,7 +7,7 @@ import com.linkmoa.source.domain.member.dto.response.MemberSimpleResponse;
 public class MemberSearchRequestDto {
 
 	public record Request(
-		String Keyword
+		String keyword
 	) {
 
 	}
