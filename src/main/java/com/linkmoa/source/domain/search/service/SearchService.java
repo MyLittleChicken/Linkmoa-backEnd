f5df0b73da16a4546bad4fb9162acfdf909b0f14
parkjihyeon124594 <91263647+parkjihyeon124594@users.noreply.github.com>
2025-04-29 16:14:49 +0900

    LP-186 feat: 멤버 검색을 위한 searchMembersByEmailOrNickname 메서드 추가

--- src/main/java/com/linkmoa/source/domain/search/service/SearchService.java
+++ src/main/java/com/linkmoa/source/domain/search/service/SearchService.java
@@ -104,7 +104,7 @@ public class SearchService {
 
 	public MemberSearchRequestDto.Response searchMembersByEmailOrNickname(
 		MemberSearchRequestDto.Request request) {
-		List<MemberSimpleResponse> memberSimpleResponses = memberDataAccess.searchByKeyword(request.Keyword());
+		List<MemberSimpleResponse> memberSimpleResponses = memberDataAccess.searchByKeyword(request.keyword());
 
 		return new MemberSearchRequestDto.Response(memberSimpleResponses);
 
