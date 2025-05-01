893f3721eaa02427cfb57ba0aee3e2e78ad01d24
parkjihyeon124594 <91263647+parkjihyeon124594@users.noreply.github.com>
2025-05-01 15:11:03 +0900

    LP-188 feat: 알람 목록 조회 시, sender 정보를 담은 NotificationSenderInfo 추가

---
+++ src/main/java/com/linkmoa/source/domain/dispatch/dto/response/NotificationSenderInfo.java
@@ -0,0 +1,14 @@
+package com.linkmoa.source.domain.dispatch.dto.response;
+
+import java.time.LocalDateTime;
+
+import lombok.Builder;
+
+@Builder
+public record NotificationSenderInfo(
+	String email,
+	String colorCode,
+	LocalDateTime sentAt,
+	String message
+) {
+}
