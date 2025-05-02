a14c17e6ab1051f883014f20c0d25a9abdf208f8
parkjihyeon124594 <91263647+parkjihyeon124594@users.noreply.github.com>
2025-05-02 12:01:58 +0900

    LP-190 feat: 쿼리 파라미터를 DTO로 변환하는 PageParameterResolver 추가

---
+++ src/main/java/com/linkmoa/source/domain/page/resolver/PageParameterResolver.java
@@ -0,0 +1,21 @@
+package com.linkmoa.source.domain.page.resolver;
+
+import org.springframework.stereotype.Component;
+
+import com.linkmoa.source.global.constant.CommandType;
+import com.linkmoa.source.global.dto.request.BaseRequest;
+
+@Component
+public class PageParameterResolver {
+
+	/*
+	createFrom(...)	명시적이며 팩토리 느낌
+ 	resolveFrom(...)	"입력으로부터 객체를 만들어낸다"는 의미 + 리졸버 컨텍스트에 잘 어울림
+	buildFrom(...)	Builder 느낌, 복잡한 조합일 때
+	convert(...)	변환 느낌, DTO → Entity처럼 매핑할 때 자주 사용
+	 */
+	public BaseRequest createBaseRequestFrom(Long pageId, CommandType commandType) {
+		return new BaseRequest(pageId, commandType);
+	}
+
+}
