d6c03175a4653c0e5069ce9f46d63088f98f312e
parkjihyeon124594 <91263647+parkjihyeon124594@users.noreply.github.com>
2025-05-03 14:29:45 +0900

    LP-192 refactor: 공유 페이지 목록조회 dto 구조 개선 및 기존 Response 클래스 제거

--- src/main/java/com/linkmoa/source/domain/page/dto/request/SharePageMembersDropdownDto.java
+++ src/main/java/com/linkmoa/source/domain/page/dto/request/SharePageMembersDropdownDto.java
@@ -1,2 +1,21 @@
-package com.linkmoa.source.domain.page.dto.request;public class SharePageMembersDropdownDto {
+package com.linkmoa.source.domain.page.dto.request;
+
+import java.util.List;
+
+import com.linkmoa.source.domain.member.dto.response.MemberSimpleResponse;
+import com.linkmoa.source.global.dto.request.BaseRequest;
+
+public class SharePageMembersDropdownDto {
+
+	public record Request(
+		BaseRequest baseRequest
+	) {
+
+	}
+
+	public record Response(
+		List<MemberSimpleResponse> members
+	) {
+
+	}
 }
