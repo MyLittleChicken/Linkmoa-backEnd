8be9c5c946f0ece10467ed384a4727ccf81440ba
parkjihyeon124594 <91263647+parkjihyeon124594@users.noreply.github.com>
2025-05-03 13:47:47 +0900

    LP-192 feat: 공유 페이지 멤버 목록 조회용 응답 dto 추가

--- src/main/java/com/linkmoa/source/domain/page/dto/response/SharePageMembersDropdownResponse.java
+++ src/main/java/com/linkmoa/source/domain/page/dto/response/SharePageMembersDropdownResponse.java
@@ -1,2 +1,10 @@
-package com.linkmoa.source.domain.page.dto.response;public class SharePageMembersDropdownResponse {
+package com.linkmoa.source.domain.page.dto.response;
+
+import java.util.List;
+
+import com.linkmoa.source.domain.member.dto.response.MemberSimpleResponse;
+
+public record SharePageMembersDropdownResponse(
+	List<MemberSimpleResponse> members
+) {
 }
