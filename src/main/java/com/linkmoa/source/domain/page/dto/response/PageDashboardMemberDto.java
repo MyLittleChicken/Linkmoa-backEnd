b007ff42d2879807806db0dabb504abfb1d75d33
parkjihyeon124594 <91263647+parkjihyeon124594@users.noreply.github.com>
2025-05-05 14:25:48 +0900

    LP-194 feat: PageDashboardMemberDto에 colorCode 필드 추가 및 isWaiting 타입 수정

--- src/main/java/com/linkmoa/source/domain/page/dto/response/PageDashboardMemberDto.java
+++ src/main/java/com/linkmoa/source/domain/page/dto/response/PageDashboardMemberDto.java
@@ -1,15 +1,12 @@
 package com.linkmoa.source.domain.page.dto.response;
 
-import lombok.Builder;
-
-@Builder
 public record PageDashboardMemberDto(
 	Long memberId,
 	String email,
 	String nickName,
 	String colorCode,
 	String role,
-	boolean isWaiting
+	Boolean isWaiting
 ) {
 
 }
